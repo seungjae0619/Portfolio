8396648501c2876330209322f1e296690d05e1e4
Park Seungjae <andrewpark0619@gmail.com>
2025-03-18 22:34:17 +0900

    히어로 페이지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 import { Github, Linkedin, Mail } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/hero-section";
 
 export default function Portfolio() {
   return (
     <div className="min-h-screen bg-[url(/background.jpg)]">
-      <header className="sticky top-0 z-40 w-full border-b border-border/10  backdrop-blur ">
+      <header className="sticky top-0 z-40 w-full border-b border-gray-400 backdrop-blur ">
         <div className="container mx-auto flex h-16 items-center justify-between">
           {/* 왼쪽 - 이름 */}
           <div className="flex items-center">
@@ -16,40 +17,49 @@ export default function Portfolio() {
           </div>
 
           {/* 중앙 - 네비게이션 */}
-          <nav className="hidden lg:flex items-center gap-4 ">
+          <nav className="hidden md:flex items-center gap-4 ">
             <Link
               href="#about"
-              className="text-sm font-medium text-[#2D3748] hover:text-[#e9e9e9] transition-colors"
+              className="text-sm font-medium text-[#2D3748] hover:text-[#9a9da1] transition-colors"
             >
               About
             </Link>
             <Link
               href="#personal"
-              className="text-sm font-medium text-[#2D3748] hover:text-[#e9e9e9] transition-colors"
+              className="text-sm font-medium text-[#2D3748] hover:text-[#9a9da1] transition-colors"
             >
               Personal
             </Link>
             <Link
               href="#projects"
-              className="text-sm font-medium text-[#2D3748] hover:text-[#e9e9e9] transition-colors"
+              className="text-sm font-medium text-[#2D3748] hover:text-[#9a9da1] transition-colors"
             >
               Projects
             </Link>
             <Link
               href="#skills"
-              className="text-sm font-medium text-[#2D3748] hover:text-[#e9e9e9] transition-colors"
+              className="text-sm font-medium text-[#2D3748] hover:text-[#9a9da1] transition-colors"
             >
               Skills
             </Link>
             <Link
               href="#contact"
-              className="text-sm font-medium text-[#2D3748] hover:text-[#e9e9e9] transition-colors"
+              className="text-sm font-medium text-[#2D3748] hover:text-[#9a9da1] transition-colors"
             >
               Contact
             </Link>
           </nav>
         </div>
       </header>
+
+      <main className="container mx-auto py-12 space-y-24">
+        <section
+          id="about"
+          className="flex flex-col items-center justify-center h-screen"
+        >
+          <HeroSection />
+        </section>
+      </main>
     </div>
   );
 }
